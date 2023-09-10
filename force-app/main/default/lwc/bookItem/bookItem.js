138df64159591192045a2b321fbbf844b7d4ea0b
Amos Go <charles79go@gmail.com>
2023-09-10 17:32:36 -0400

    search working

--- force-app/main/default/lwc/bookItem/bookItem.js
+++ force-app/main/default/lwc/bookItem/bookItem.js
@@ -3,12 +3,22 @@ import { LightningElement, api } from 'lwc';
 export default class BookItem extends LightningElement {
     @api bookItem;
 
-    previewBookFn(event){
+    get titleRank() {
+        return this.bookItem.rank
+            ? `#${this.bookItem.rank} - ${this.bookItem.title}`
+            : this.bookItem.title;
+    }
+
+    previewBookFn(event) {
         event.preventDefault();
-        this.dispatchEvent(new CustomEvent('previewbook', {detail: {
-            title: this.bookItem.title,
-            author: this.bookItem.author,
-            primary_isbn10: this.bookItem.primary_isbn10
-        }}));
+        this.dispatchEvent(
+            new CustomEvent('previewbook', {
+                detail: {
+                    title: this.bookItem.title,
+                    author: this.bookItem.author,
+                    primary_isbn10: this.bookItem.primary_isbn10
+                }
+            })
+        );
     }
 }
